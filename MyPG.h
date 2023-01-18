6f0d58123348b1918970ff1a93fe710ad4522f24
やまもと <99802855+NakamuraYuuki02@users.noreply.github.com>
2023-01-18 11:29:10 +0900

    Revert "Revert "Select master 1/11""

--- MyPG.h
+++ MyPG.h
@@ -83,14 +83,14 @@ namespace  MyPG
 		bool returnFlag = false;//タイトルに戻るフラグ
 
 		//選択された情報を入れる変数
-		//武器 初回に選択
+		//武器 初回に一つ選択
 		enum class SelectedWeapon
 		{
 			Sword,
 			Axe,
 			Gun
 		};
-		SelectedWeapon selectedWeapon;
+		SelectedWeapon sw;
 		//スキル 毎回二つ選択
 		enum class SelectedSkill
 		{
@@ -99,14 +99,22 @@ namespace  MyPG
 			HpUp,
 			AtkUp,
 			Special1,
-			Special2
+			Special2,
+			Non
 		};
-		vector<SelectedSkill> selectedSkill;
+		vector<SelectedSkill> ss;
+
+		//スキル選択数 selected skill number
+		int ssn;
+
 		//ステージ 毎回一つ選択
 		int selectedStage;
 
-		//ステージ通過数、ゲーム進行度
+		//ステージ数、ステージ通過数(ゲーム進行度)
 		int stageNum;
+
+		//経過したステージ 一つ前のステージの番号
+		int eapsedStage;
 
 		//プレイヤーステータス
 		int hp;				//体力
